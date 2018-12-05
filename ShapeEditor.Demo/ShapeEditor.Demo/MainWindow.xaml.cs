51f158e03d8eeb6ce5cbfbb45b2a28102c4fc6ed
Petr Voborník <PetrVobornik@users.noreply.github.com>
2018-12-05 19:11:15 +0100

    Add files via upload

---
+++ ShapeEditor.Demo/ShapeEditor.Demo/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Amporis.WPF.ShapeEditor.Demo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Shape_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            ShapeEditorControl.CaptureElement(sender as FrameworkElement, e);
+            e.Handled = true;
+        }
+
+        private void MainArea_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            ShapeEditorControl.ReleaseElement();
+        }
+    }
+}
